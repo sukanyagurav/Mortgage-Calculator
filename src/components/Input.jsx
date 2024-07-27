40eaeee7604eb3218bc610f9669845e3d9e7a0bd
sukanya gurav <sukanyagurav6@gmail.com>
2024-07-27 22:09:46 +0530

    UPDATED CSS

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -7,8 +7,8 @@ const Input = (({text,position,padding,errors,register,name,valueAsNumber,min,ma
     }
   }
   return (
-    <>    <div className={` rounded-md border-[1.5px] overflow-hidden border-slate_500 relative inputs ${errors && ' border-red'}`}>
-        <span className={`${position} absolute bg-slate_100 font-bold  p-3 py-2 ${errors && 'bg-red '} `}>{text}</span>
+    <>    <div className={` rounded-md border-[1.5px] overflow-hidden relative inputs ${errors ?  'border-red' : 'border-slate_500'}`}>
+        <span className={`${position} absolute  font-bold  p-3 py-2 ${errors ?  'bg-red' : 'bg-slate_100'} `}>{text}</span>
         <input type="number" className={`w-full bg-transparent transition-all outline-none  font-inherit  ${padding} block text-left py-2`} name={name} onKeyDown={onInput} {...register(name,{valueAsNumber,require:true})} min={min} max={max}/>
        
     </div>
